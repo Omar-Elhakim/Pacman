72cc6821386c7b945b327d8be04f672293e4ad7e
Hakimos7777 <omarelhakim@icloud.com>
2024-04-15 05:19:13 +0200

    adjusting to cisual studio

--- Window.cpp
+++ Window.cpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "Window.h"
-#include <raylib.h>
+#include "raylib.h"
 
-Window::Window(int Width, int Height, char *title) {
+
+Window::Window(int Width, int Height) {
     this->Width = Width;
     this->Height = Height;
     SetTraceLogLevel(LOG_NONE);
-    InitWindow(Width, Height, title);
+    InitWindow(Width, Height, "Mapmaker");
     SetTargetFPS(60);
     SetWindowState(FLAG_WINDOW_RESIZABLE);
 }
