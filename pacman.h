00afbe10bb865c3b02591b96c613e2e01cca31c1
OmarTebry <omartebry@gmail.com>
2024-04-17 03:45:22 +0200

    sha8alt el sho8l

---
+++ pacman.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "raylib.h"
+class pacman
+{
+public:
+	pacman();
+	static void funco(Texture2D pacman1, Vector2 position, Vector2 direction, Rectangle box, Image pac, float speed,float x, int a );
+	static void moveright(Vector2 p,Vector2 d,Rectangle r, Image pacman,float speed, float x, int a);
+	static void moveleft(Vector2 p, Vector2 d, Rectangle r, Image pacman, float speed, float x, int a);
+	static void moveup(Vector2 p, Vector2 d, Rectangle r, Image pacman, float speed, float x, int a);
+	static void movedown(Vector2 p, Vector2 d, Rectangle r, Image pacman, float speed, float x, int a);
+};
+
