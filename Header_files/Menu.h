a92ed8b29710b41434f2196e6ffaf74c72d81f43
ahmed <a7med.mostafa2323@gmail.com>
2024-05-11 14:33:51 +0300

    added "create ur own map" button in the main menu added win/lose screens

--- Header_files/Menu.h
+++ Header_files/Menu.h
@@ -8,4 +8,15 @@ void mainMenu();
 void toStartMenu(Texture2D background, Texture2D logo, Font customFont, Texture2D arrowTexture);
 void toHowToPlay(Texture2D background, Texture2D logo, Font customFont, Texture2D arrowTexture);
 void toCredits(Texture2D background, Texture2D logo, Font customFont, Texture2D arrowTexture);
+void createMap(Texture2D background, Texture2D logo, Font customFont, Texture2D arrowTexture);
+void WinScreen(Texture2D background, Font customFont);
+void LoseScreen(Texture2D background, Font customFont);
+// Custom drawing function
+struct CustomButton {
+    Rectangle bounds;
+    Color color;
+    const char* text;
+    int fontSize;
+};
+void DrawCustomButton(CustomButton button);
 #endif
